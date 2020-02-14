29c06e05f6f5788c542982d454ef770c641a3f91
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2020-02-14 16:32:16 +0100

    Add project files.

---
+++ Datacardio.test/TestCuore.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Datacardio.test
+{
+    [TestClass]
+    public class TestCuore
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
